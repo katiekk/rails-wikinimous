b42254948422948d19c0c916229e023cc2157077
Katie Lin <katiekklht@gmail.com>
2021-11-09 15:10:50 +0000

    Add scss to match the style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,41 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(strong_params)
+    redirect_to articles_path
+  end
+
+  def edit; end
+
+  def update
+    @article.update(strong_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, flash: { notice: 'Article was successfully destroyed.' }
+  end
+
+  private
+
+  def strong_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
 end
